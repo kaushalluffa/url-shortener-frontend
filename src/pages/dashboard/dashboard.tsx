113693e342246c33e9f6e952e8bb095c5138de91
kaushalluffa <kaushalluffa91@gmail.com>
2025-03-23 14:12:21 -0400

    feat: add tanstack table for urls list

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,84 @@
+import { Plus } from "lucide-react";
+import { FormEvent } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import { useUrl } from "../../context/url-context";
+import { urlTableColumns } from "./columns";
+import { UrlDataTable } from "./data-table";
+import { useToast } from "../../hooks/use-toast";
+import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+
+export default function DashboardPage() {
+  const { urlsData, setPagination, pagination } = useUrl();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const { toast } = useToast();
+  const tableColumns = urlTableColumns(toast);
+  const table = useReactTable({
+    data: urlsData?.listOfUrls || [],
+    columns: tableColumns,
+    getCoreRowModel: getCoreRowModel(),
+    rowCount: urlsData?.totalCount,
+    state: { pagination },
+    onPaginationChange: setPagination,
+    manualPagination: true,
+  });
+  function searchSubmitHandler(e: FormEvent) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const searchTerm = formData.get("search") as string;
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("page", "1");
+    newSearchParams.set("search", searchTerm);
+    navigate(`?${newSearchParams.toString()}`);
+    setPagination({ pageIndex: 0, pageSize: 10 });
+  }
+  function goToNextPage() {
+    const page = Number(searchParams.get("page") || 1) + 1 || 1;
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("page", String(page));
+    navigate(`?${newSearchParams.toString()}`);
+    table.nextPage();
+  }
+  function goToPreviousPage() {
+    const page = Number(searchParams.get("page") || 1) - 1 || 1;
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("page", String(page));
+    navigate(`?${newSearchParams.toString()}`);
+    table.previousPage();
+  }
+  return (
+    <div className="space-y-6">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <Button asChild>
+          <Link to="/create">
+            <Plus className="mr-2 h-4 w-4" />
+            Create Short URL
+          </Link>
+        </Button>
+      </div>
+
+      <form onSubmit={searchSubmitHandler}>
+        <div className="flex items-center space-x-2">
+          <Input
+            placeholder="Search URLs by Original URL..."
+            className="max-w-sm"
+            name="search"
+          />
+          <Button type="submit">Search</Button>
+        </div>
+      </form>
+
+      <div className="rounded-md border overflow-x-auto">
+        <UrlDataTable
+          table={table}
+          goToNextPage={goToNextPage}
+          goToPreviousPage={goToPreviousPage}
+          columnsLength={tableColumns.length}
+        />
+      </div>
+    </div>
+  );
+}
